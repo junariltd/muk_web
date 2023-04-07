28c8081bb6512c69c7d0db561b331e7254ee0439
Russell Briggs <russell.briggs@junari.com>
2023-04-07 11:29:17 +0000

    [FIX] CMS issue caused by expanding menu overrides

--- muk_web_theme/static/src/webclient/appsbar/appsbar.js
+++ muk_web_theme/static/src/webclient/appsbar/appsbar.js
@@ -49,25 +49,19 @@ import { Component } from "@odoo/owl";
 import { ActionContainer } from '@web/webclient/actions/action_container'
 import { WebsitePreview } from '@website/client_actions/website_preview/website_preview'
 import { patch } from "@web/core/utils/patch";
-import { xml, EventBus, useState, onWillUnmount } from "@odoo/owl";
-
+import { xml, EventBus, useState } from "@odoo/owl";
 
 // Create an Odoo service for communication between components
-
 export const AppBarService = {
     dependencies: [],
     start(env) {
-
         const bus = new EventBus();
-
         function registerEventHandler(event, fnct) {
             bus.addEventListener(event, fnct);
         }
-
         function triggerEvent(event) {
             bus.trigger(event);
         }
-
         return {
             registerEventHandler: registerEventHandler.bind(this),
             triggerEvent: triggerEvent.bind(this),
@@ -86,34 +80,37 @@ export class AppsBar extends Component {
             expanded: false,
             is_hidden: false
         })
-
         this.appbar_svc = useService("muk_appbar");
-        this.appbar_svc.registerEventHandler('expand_bar', () => this._expandMenu());
-        this.appbar_svc.registerEventHandler('collapse_bar', () => this._collapseMenu());
-        this.appbar_svc.registerEventHandler('hide_bar', () => this._hideMenu());
-        this.appbar_svc.registerEventHandler('show_bar', () => this._showMenu());
+        this.appbar_svc.registerEventHandler('expand_bar', () => this._expandAppbar());
+        this.appbar_svc.registerEventHandler('collapse_bar', () => this._collapseAppbar());
+        this.appbar_svc.registerEventHandler('hide_bar', () => this._hideAppbar());
+        this.appbar_svc.registerEventHandler('show_bar', () => this._showAppbar());
     }
     onMouseMove(ev) {
         let current_position = ev.clientX
         if(current_position > 0 && current_position <= 20) {
-            this._expandMenu();
+            this._expandAppbar();
         }
     }
-    _collapseMenu() {
+    _collapseAppbar() {
         if (this.state.expanded) {
             this.state.expanded = false;
         }
     }
-    _expandMenu() {
+    _expandAppbar() {
         if (!this.state.expanded) {
             this.state.expanded = true;
         }
     }
-    _hideMenu() {
-        this.state.is_hidden = true;
+    _hideAppbar() {
+        if (!this.state.is_hidden) {
+            this.state.is_hidden = true;
+        }
     }
-    _showMenu() {
-        this.state.is_hidden = false;
+    _showAppbar() {
+        if (this.state.is_hidden) {
+            this.state.is_hidden = false;
+        }
     }
 }
 Object.assign(AppsBar, {
@@ -124,49 +121,51 @@ Object.assign(AppsBar, {
 });
 
 
-// Override Odoo's "ActionContainer" component, so when a user moves the mouse outside of the navbar
-// over the main Odoo window, it triggers the 'collapse_bar' event on the AppBarService
-
+/* Override Odoo's "ActionContainer" component:
+ *  - When the user moves the mouse over the main action window, trigger the navbar to collapse
+ *  - When the navbar is shown or hidden, adjust the action manager left padding accordinly
+*/
 patch(ActionContainer.prototype, 'muk_web_theme.ActionContainer', {
     setup() {
-        this._super(...arguments);
-        this.state = useState({
-            appbar_is_hidden: false
+        const res = this._super(...arguments);
+        this.appbar_state = useState({
+            remove_appbar_padding: false
         })
         this.appbar_svc = useService("muk_appbar");
-        this.appbar_svc.registerEventHandler('hide_bar', () => this._hideMenu());
-        this.appbar_svc.registerEventHandler('show_bar', () => this._showMenu());
+        this.appbar_svc.registerEventHandler('hide_bar', () => this._removeAppbarPadding());
+        this.appbar_svc.registerEventHandler('show_bar', () => this._addAppbarPadding());
+        this.onNewAction = this.onNewAction.bind(this);
+        this.env.bus.addEventListener("ACTION_MANAGER:UPDATE", this.onNewAction);
+        return res;
     },
-    _hideMenu() {
-        this.state.appbar_is_hidden = true;
+    _removeAppbarPadding() {
+        this.appbar_state.remove_appbar_padding = true;
     },
-    _showMenu() {
-        this.state.appbar_is_hidden = false;
+    _addAppbarPadding() {
+        this.appbar_state.remove_appbar_padding = false;
     },
     onMainWindowMouseMove() {
         this.appbar_svc.triggerEvent('collapse_bar');
+    },
+    onNewAction() {
+        let component = this.info.Component;
+        if (component && component.Component) {
+            component = component.Component;
+        }
+        if (component == WebsitePreview) {
+            this.appbar_svc.triggerEvent('hide_bar');
+        }
+        else {
+            setTimeout(() => { // delay slightly so it doesn't load before the main view
+                this.appbar_svc.triggerEvent('show_bar');
+            }, 300);
+        }
     }
 });
 ActionContainer.template = xml`
     <t t-name="web.ActionContainer">
       <div class="o_action_manager" t-on-mousemove="onMainWindowMouseMove"
-        t-att-style="state.appbar_is_hidden ? 'padding-left: 0px;' : ''">
+        t-att-style="appbar_state.remove_appbar_padding ? 'padding-left: 0px;' : ''">
         <t t-if="info.Component" t-component="info.Component" className="'o_action'" t-props="info.componentProps" t-key="info.id"/>
       </div>
     </t>`;
-
-// Override Odoo's "ActionContainer" component, so when a user moves the mouse outside of the navbar
-// over the main Odoo window, it triggers the 'collapse_bar' event on the AppBarService
-
-patch(WebsitePreview.prototype, 'muk_web_theme.ActionContainer', {
-    setup() {
-        const res = this._super(...arguments);
-        this.appbar_svc = useService("muk_appbar");
-        this.appbar_svc.triggerEvent('hide_bar');
-        onWillUnmount(this.onWillUnmount);
-        return res;
-    },
-    onWillUnmount() {
-        this.appbar_svc.triggerEvent('show_bar');
-    }
-});
